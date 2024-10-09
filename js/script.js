35c6f77c6ca38b2d8c2b16edaf2c4f4f1ffcb2a5
Bermelio <facundoperalta258@gmail.com>
2024-10-09 06:02:46 -0300

    stage 2, Semi Crud

--- js/script.js
+++ js/script.js
@@ -1,43 +1,102 @@
-//array main notes,,
+// ============== array main notes =================
 const arrayNotes = [
-    {
-        id:1,
-        title:'La mama del mati',
-        content:'uhhh guacho la mama del mati y la prima de ja xd',
-        date:'17/10/2000',
-        img:'./assests/maestroSplinter.jpg'
-   },
-   {
-        id:2,
-        title:'Minecraft',
-        content:'hacer granja de experiencia',
-        date:'07/10/2005',
-        img:'./assests/floppa.jpg'
-   },
-   {
-        id:3,
-        title:'boviano ataca denuevo',
-        content:'skibidid dub dub yes ye sskibidi dip dip',
-        date:'',
-        img:'./assests/rei.jpg'
-   }
-];
-
-//second stage logic
-
-const containerNotes = document.getElementById('container-notes')
-
-arrayNotes.forEach((note) =>{
-
-    const div = document.createElement('div')
-
-    div.classList.add('notesClass')
-
-    div.innerHTML =`
-    <h3>${note.title} n°${note.id}</h3>
-    <p>${note.content}</p>
-    <br>
-    <img src="${note.img}">
-    `
-    containerNotes.appendChild(div)
-})
+     {
+         id: 1,
+         title: 'La mama del mati',
+         content: 'uhhh guacho la mama del mati y la prima de ja xd',
+         img: ''
+     },
+     {
+         id: 2,
+         title: 'Minecraft',
+         content: 'hacer granja de experiencia',
+         img: ''
+     },
+     {
+         id: 3,
+         title: 'boviano ataca denuevo',
+         content: 'skibidid dub dub yes ye sskibidi dip dip',
+         img: ''
+     },
+     {
+         id: 4,
+         title: 'fruta',
+         content: 'comprar mandzanada y esto y ayqeuiasd',
+         img: ''
+     }
+ ];
+ 
+ // =================== random img ================= 
+ const collectionImg = [
+     'assests/argentina.jpg',
+     'assests/fiat-uno.jpg',
+     'assests/floppa.jpg',
+     'assests/fulvo.jpg',
+     'assests/maestroSplinter.jpg',
+     'assests/mondongo.jpg',
+     'assests/rei.jpg',
+     'assests/skeleto.jpg'
+ ];
+ 
+ function randomImg(collectionImg) {
+     const randomIndex = Math.floor(Math.random() * collectionImg.length);
+     return collectionImg[randomIndex];
+ }
+ 
+ // ========= render total =========
+ const containerNotes = document.getElementById('containerNotes');
+ 
+ function renderNotes() {
+     containerNotes.innerHTML = '';
+     arrayNotes.forEach((note) => {
+         const div = document.createElement('div');
+         div.classList.add('notesClass');
+         div.innerHTML = `
+             <h3>${note.title} n°${note.id}</h3>
+             <p>${note.content}</p>
+             <br>
+             <img src="${note.img}">
+         `;
+         containerNotes.appendChild(div);
+     });
+ }
+ 
+ arrayNotes.forEach(note => {
+     note.img = randomImg(collectionImg);
+ });
+ 
+ renderNotes();
+ 
+ // ============== function ====================  
+ const newNote = document.getElementById('newNote');
+ const btnClose = document.getElementById('btnClose');
+ 
+ newNote.addEventListener('mouseover', () => {
+     modalContainer.classList.add('modalToggle');
+ });
+ 
+ btnClose.addEventListener('click', () => {
+     modalContainer.classList.remove('modalToggle');
+ });
+ 
+ // ========================  Fill form ===================
+ const title = document.getElementById('title');
+ const content = document.getElementById('text');
+ const btnSave = document.getElementById('btnSave');
+ 
+ btnSave.addEventListener('click', (e) => {
+     e.preventDefault();
+ 
+     const pushNote = {
+         id: arrayNotes.length + 1,
+         title: title.value,
+         content: content.value,
+         img: randomImg(collectionImg)
+     };
+ 
+     arrayNotes.push(pushNote);
+     renderNotes();
+     title.value = '';
+     content.value = '';
+ });
+ 
